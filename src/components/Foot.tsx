3cc041d72b897a5e7b33a539a21ea140d87137d3
Rudy Quinternet <rudy.quinternet@gmail.com>
2022-11-15 21:38:14 +0100

    vio let

--- src/components/Foot.tsx
+++ src/components/Foot.tsx
@@ -1,10 +1,10 @@
-import logo from 'media/logo.svg';
+import logo from 'media/logo-react.png';
 
 function Foot() {
     return (
         <div className="App-footer">
             <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{margin: "25px"}}>
           made via <span style={{color: "#61dbfb"}}>React</span>  with 💜
         </p>
         </div>
