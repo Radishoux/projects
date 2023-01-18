576d7542ca2c0e8c04c17fcbe0341752cc90a092
Rudy Quinternet <rudy.quinternet@gmail.com>
2023-01-18 17:37:18 +0100

    add pyra and clean a little

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,5 @@
 import RudyCard from "components/Card";
+import pyraLogo from "media/pyra-logo.png";
 import rudyFace from "media/rudyFace.png";
 import rudyFolioScreen from "media/rudyFolioScreen.png";
 
@@ -7,24 +8,24 @@ function Body() {
         <div className="App-body">
             <RudyCard
             cardTitle={"About me"}
-            editDate={"november 2022"}
+            editDate={"october 2022"}
             cardImage={rudyFolioScreen}
             cardLink={"https://radishoux.github.io/folio"}
             cardDescription={"My resume website"}/>
 
             <RudyCard
-            cardTitle={"About me"}
-            editDate={"november 2022"}
-            cardImage={rudyFace}
-            cardLink={"https://rudyquinternet.com/"}
-            cardDescription={"My resume website"}/>
-
+            cardTitle={"Pyra"}
+            editDate={"december 2022"}
+            cardImage={pyraLogo}
+            cardLink={""}
+            cardDescription={"A study on the ponzi scheme and how would nowadays society use it"}/>
+{/*
             <RudyCard
             cardTitle={"About me"}
             editDate={"november 2022"}
             cardImage={rudyFace}
             cardLink={"https://rudyquinternet.com/"}
-            cardDescription={"My resume website"}/>
+            cardDescription={"My resume website"}/> */}
         </div>
     );
 }
