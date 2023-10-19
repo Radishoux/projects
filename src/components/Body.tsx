07e2fb2d0b72f37422364ea90d29f275673fea8c
Rudy Quinternet <rudy.quinternet@gmail.com>
2023-10-19 15:15:36 +0200

    remove pyra

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,4 @@
 import RudyCard from "components/Card";
-import pyraLogo from "media/pyra-logo.png";
-import rudyFace from "media/rudyFace.png";
 import rudyFolioScreen from "media/rudyFolioScreen.png";
 
 function Body() {
@@ -12,13 +10,13 @@ function Body() {
             cardImage={rudyFolioScreen}
             cardLink={"https://radishoux.github.io/folio"}
             cardDescription={"My resume website"}/>
-
+{/*
             <RudyCard
             cardTitle={"Pyra"}
             editDate={"december 2022"}
             cardImage={pyraLogo}
             cardLink={""}
-            cardDescription={"A study on the ponzi scheme and how would nowaday's society use it"}/>
+            cardDescription={"A study on the ponzi scheme and how would nowaday's society use it"}/> */}
 {/*
             <RudyCard
             cardTitle={"About me"}
